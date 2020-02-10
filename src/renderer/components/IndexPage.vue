6e79def1c5aa18dab7c4d0742415f6c4fc7dd61f
mgsong <mgsong@jjsoft.kr>
2020-02-11 06:33:07 +0900

    add sweetalert2

--- src/renderer/components/IndexPage.vue
+++ src/renderer/components/IndexPage.vue
@@ -2,20 +2,34 @@
   <main>
     <section>
       <div class="title">
-        <h1>어린이부 출석체크</h1>
+        <h1>어린이부 출석체크({{today}})</h1>
       </div>
-      <input class="input" v-model="input" id="code" onkeyup="check()" @keyup.enter="check" />
+      <input ref="input" class="input" v-model="input" id="code" @keyup.enter="check" />
       <div class="result">
-        <h2 v-for="(item, idx) in list" :key="idx">{{item}}</h2>
+        <div class="item" v-for="(item, idx) in list" :key="idx">
+          <div class="name">{{item.name}}</div>-
+          <span>{{item.time}}</span>
+        </div>
       </div>
     </section>
   </main>
 </template>
 <script>
+import {codeMap} from '../../biz/codeMap'
+import moment from 'moment'
+import Swal from 'sweetalert2'
+
 export default {
   name: 'index-page',
   data: () => {
-    return {list: [], input: ''}
+    return {
+      list: [{name: '송하니', time: '12:12:12'}],
+      input: '',
+      today: moment().format('YYYY-MM-DD'),
+    }
+  },
+  mounted() {
+    this.$refs.input.focus()
   },
   methods: {
     // onlyNumber() {
@@ -24,8 +38,27 @@ export default {
     //   }
     // },
     check() {
-      this.list = [this.input, ...this.list]
+      const name = codeMap[this.input]
+      if (!name) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops..',
+          text: 'The name of code is not founddwd',
+          showConfirmButton: true,
+          timer: 3000,
+        })
+        this.input = ''
+        return
+      }
+      this.list = [{name, time: moment().format('HH:mm:ss')}, ...this.list]
       this.input = ''
+      Swal.fire({
+        icon: 'success',
+        title: 'Welcome ' + name + ':)',
+        text: '출석체크 완료~*',
+        showConfirmButton: true,
+        timer: 3000,
+      })
     },
   },
 }
@@ -44,6 +77,15 @@ main {
     width: 400px;
     height: 500px;
     margin-top: 20px;
+    .item {
+      .name {
+        display: inline-block;
+        width: 100px;
+        font-size: 24px;
+        font-weight: bold;
+        color: #555;
+      }
+    }
   }
 }
 </style>
