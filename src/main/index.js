77747e3233d804991aa269a1f771d1f78cf03e2c
mgsong <mgsong@jjsoft.kr>
2020-02-14 21:29:56 +0900

    prod 에서도 dev-tools 설치

--- src/main/index.js
+++ src/main/index.js
@@ -1,71 +1,27 @@
-'use strict'
-
-import {app, BrowserWindow} from 'electron'
-
 /**
- * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
- */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path')
-    .join(__dirname, '/static')
-    .replace(/\\/g, '\\\\')
-}
-
-let mainWindow
-const winURL =
-  process.env.NODE_ENV === 'development'
-    ? `http://localhost:9080`
-    : `file://${__dirname}/index.html`
-
-function createWindow() {
-  /**
-   * Initial window options
-   */
-  mainWindow = new BrowserWindow({
-    height: 563,
-    useContentSize: true,
-    width: 1000,
-  })
-
-  mainWindow.loadURL(winURL)
-  mainWindow.webContents.openDevTools()
-
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ * This file is used specifically and only for development. It installs
+ * `electron-debug` & `vue-devtools`. There shouldn't be any need to
+ *  modify this file, but it can be used to extend your development
+ *  environment.
  */
 
-/*
-import { autoUpdater } from 'electron-updater'
+/* eslint-disable */
 
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
+// Install `electron-debug` with `devtron`
+require('electron-debug')({showDevTools: false})
+
+// Install `vue-devtools`
+require('electron').app.on('ready', () => {
+  let installExtension = require('electron-devtools-installer')
+  installExtension
+    .default(installExtension.VUEJS_DEVTOOLS)
+    .then(() => {
+      console.log('VUEJS_DEVTOOLS is installed')
+    })
+    .catch(err => {
+      console.log('Unable to install `vue-devtools`: \n', err)
+    })
 })
 
-app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
- */
+// Require `main` process to boot app
+require('./index.origin')
